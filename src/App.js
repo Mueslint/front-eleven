5ca037a4071e7e54fe01ad60aea4f42ae70ea767
Mueslint <solom.emmanuel@gmail.com>
2019-08-31 20:04:20 +0200

    Nearly everything working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import AstronautsList from './Components/AstronautsList'
+import TestForm from './Components/TestForm'
 
 function App() {
+  const [astronauts, setAstronauts] = useState([]);
+
+  const fetchAstronauts = async () => {
+    const response = await axios.get(`http://127.0.0.1:3300/getAstronauts`);
+    setAstronauts(response.data);
+  };
+
+  const addAstronaut = (newAstronaut) => {
+    const updatedAstronauts = [...astronauts, newAstronaut];
+    setAstronauts(updatedAstronauts);
+  }
+
+  const deleteAstronaut = (id) => {
+    const afterDeletion = astronauts.filter((el, index) => {
+      return index !== id
+    })
+    setAstronauts(afterDeletion);
+  }
+
+  const editAstronaut = (editedAstronaut, id) => {
+    const afterEdition = astronauts.map((astronaut, index) => {
+      if (index !== id) {
+        return astronaut
+      } else {
+        return editedAstronaut
+      }
+    })
+    setAstronauts(afterEdition);
+  }
+
+  useEffect( () => { fetchAstronauts() },  [] );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={{textAlign: 'center', padding: 30}}>Youpi Boule</h1>
+      <div style={{display: 'flex', justifyContent: 'space-around'}}>
+        <AstronautsList
+          astronauts={astronauts}
+          deleteAstronaut={deleteAstronaut}
+          editAstronaut={editAstronaut}
+        />
+        <TestForm addAstronaut={addAstronaut}/>
+      </div>
     </div>
   );
 }
